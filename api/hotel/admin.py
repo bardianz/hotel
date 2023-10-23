b6a809ee880840123b164bf0b263d93a9f76c33c
Bardia Nikbakhsh <BardiaNikbakhsh@gmail.com>
2023-10-23 14:08:54 +0330

    update hotel admin panel

--- api/hotel/admin.py
+++ api/hotel/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Hotel,Picture
 
-admin.site.register(Hotel)
 # admin.site.register(Picture)
+
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ["name", "id", "picture_url"]
+
+admin.site.register(Hotel, HotelAdmin)
